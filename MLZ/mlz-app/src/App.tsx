506a4c93b35b028a21e90f24b513914ffc7279b7
deimantem <deimante.mielryte2@gmail.com>
2023-11-25 11:15:21 +0100

    Test fetching data from json-server

--- MLZ/mlz-app/src/App.tsx
+++ MLZ/mlz-app/src/App.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-export function App() {
+interface Entity {
+    id: number;
+    name: string;
+}
+
+const App: React.FC = () => {
+    const [data, setData] = useState<Entity[]>([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/entities');
+            const jsonData: Entity[] = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Hello world</h1>
+                <h1>Your App Header</h1>
+                <ul>
+                    {data.map(entity => (
+                        <li key={entity.id}>{entity.name}</li>
+                    ))}
+                </ul>
             </header>
         </div>
     );
-}
+};
 
 export default App;
