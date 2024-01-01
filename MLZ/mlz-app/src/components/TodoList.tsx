60d772b9038364a8ce6da0c12e53e8e97f62093d
deimantem <deimante.mielryte2@gmail.com>
2024-01-01 14:37:24 +0100

    Add search to list

--- MLZ/mlz-app/src/components/TodoList.tsx
+++ MLZ/mlz-app/src/components/TodoList.tsx
@@ -6,6 +6,7 @@ import '../styles/TodoList.css';
 
 const TodoList: React.FC = () => {
     const [todos, setTodos] = useState<Todo[]>([]);
+    const [searchTerm, setSearchTerm] = useState<string>('');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -31,14 +32,27 @@ const TodoList: React.FC = () => {
         }
     };
 
+    // Filter todos based on the title matching the search term
+    const filteredTodos = todos.filter((todo) =>
+        todo.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
         <div className="todo-list-wrapper">
             <h2 className="todo-list-title">Todo-List</h2>
+            <div className="search-container">
+                <input
+                    type="text"
+                    placeholder="Search todo by title"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
             <Link to="/create" className="create-todo-button">
                 Create New Todo
             </Link>
             <div className="card-container">
-                {todos.map((todo) => (
+                {filteredTodos.map((todo) => (
                     <div key={todo.id} className="card">
                         <Link to={`/todo/${todo.id}`}>View Details</Link>
                         <div className="card-header">
